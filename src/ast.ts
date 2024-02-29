97b255349695b53cdccabc6c203c9ebbe4a4e795
Michael Black <michael@TheLambda.group>
2024-02-29 05:30:44 -0600

    Added support for line numbers

--- src/ast.ts
+++ src/ast.ts
@@ -43,11 +43,14 @@ export class Node {
     type: TokenType;
     text: string;
     children: Node[];
+    line: number;
+    parent?: Node;
 
-    constructor(type: TokenType, text: string) {
+    constructor(type: TokenType, text: string, line: number) {
         this.type = type;
         this.text = text;
         this.children = [];
+        this.line = line;
     }
 
     formatChildren(startDepth: number): string {
@@ -82,17 +85,37 @@ export class Node {
         }
         return text;
     }
+
+    assignParents() {
+        this.children.map(child => {
+            child.parent = this;
+            child.assignParents();
+        });
+    }
 }
 
-function parseToken(token: string): Node {
+function get_line(node: Node, line: number): Node[] {
+    let nodes: Node[] = [];
+    if (node.line === line) {
+        nodes.push(node);
+    }
+    else if(node.line > line) {
+        return nodes;
+    }
+    let children = node.children.map(child => get_line(child, line));
+    nodes.concat(children.flat());
+    return nodes;
+}
+
+function parseToken(token: string, line: number): Node {
     if (isBlockOpenChar(token)) {
-        return new Node(TokenType.Open, token);
+        return new Node(TokenType.Open, token, line);
     } else if (isBlockEndChar(token)) {
-        return new Node(TokenType.Close, token);
+        return new Node(TokenType.Close, token, line);
     } else if (isWhiteSpaceChar(token)) {
-        return new Node(TokenType.Newline, token);
+        return new Node(TokenType.Newline, token, line);
     } else {
-        return new Node(TokenType.Fact, token);
+        return new Node(TokenType.Fact, token, line);
     }
 }
 
@@ -104,25 +127,29 @@ function tokenize(text: string): string[] {
 }
 
 export function parseText(text: string): Node {
-    let parentNode = new Node(TokenType.Start, "StartNode");
+    let parentNode = new Node(TokenType.Start, "StartNode", 0);
     let _ = 0;
     let tokens = tokenize(text);
-    [parentNode.children, _] = parse(parentNode, tokens, 0);
+    [parentNode.children, _, _] = parse(parentNode, tokens, 0, 0);
     let s = parentNode.formatChildren(0);
+    parentNode.assignParents();
+    let line = get_line(parentNode, 2);
     return parentNode;
 }
 
-export function parse(node: Node, tokens: string[], index: number): [Node[], number] {
+export function parse(node: Node, tokens: string[], index: number, line: number): [Node[], number, number] {
     let nodes: Node[] = [];
     for (let i = index; i < tokens.length; i++) {
-        let newNode = parseToken(tokens[i]);
+        let newNode = parseToken(tokens[i], line);
         if (newNode.type === TokenType.Open) {
-            [newNode.children, i] = parse(newNode, tokens, i + 1);
+            [newNode.children, i, line] = parse(newNode, tokens, i + 1, line);
         } else if (newNode.type === TokenType.Close) {
             nodes.push(newNode);
-            return [nodes, i];
+            return [nodes, i, line];
+        } else if (newNode.type === TokenType.Newline) {
+            line++;
         }
         nodes.push(newNode);
     }
-    return [nodes, tokens.length];
+    return [nodes, tokens.length, line];
 }

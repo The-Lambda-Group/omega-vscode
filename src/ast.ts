60ecb606a465f9deadd92e7a0fdc29e40cc20725
Michael Black <michael@TheLambda.group>
2024-02-28 21:48:28 -0600

    Added support for enforcing depth based indentation

--- src/ast.ts
+++ src/ast.ts
@@ -1,41 +1,41 @@
 function getWordChars(): string[] {
     return ['(', ')', '{', '}', '[', ']'];
-  }
-  
-  function isBlockChar(char: string): boolean {
+}
+
+function isBlockChar(char: string): boolean {
     let wordChars = getWordChars();
     return wordChars.includes(char);
-  }
-  
-  function isWhiteSpaceChar(char: string): boolean {
+}
+
+function isWhiteSpaceChar(char: string): boolean {
     let whitespaceChars = [' ', '\t', '\n'];
     return whitespaceChars.includes(char);
-  }
-  
-  function isPunctuationChar(char: string): boolean {
+}
+
+function isPunctuationChar(char: string): boolean {
     let punctuationChars = [',', ':'];
     return punctuationChars.includes(char);
-  }
-  
-  function isBlockEndChar(char: string): boolean {
+}
+
+function isBlockEndChar(char: string): boolean {
     let wordChars = [')', '}', ']'];
     return wordChars.includes(char);
-  }
-  
-  function isBlockOpenChar(char: string): boolean {
+}
+
+function isBlockOpenChar(char: string): boolean {
     let wordChars = ['(', '[', '{'];
     return wordChars.includes(char);
-  }
-  
-  function isWordOverChar(char: string): boolean {
+}
+
+function isWordOverChar(char: string): boolean {
     return isBlockChar(char) || isWhiteSpaceChar(char);
-  }
+}
 
 export enum TokenType {
     Open,
     Close,
     Fact,
-    Whitespace,
+    Newline,
     Start
 }
 
@@ -50,6 +50,32 @@ export class Node {
         this.children = [];
     }
 
+    formatChildren(startDepth: number): string {
+        let increment = 3;
+        let text = "";
+        let leadingWhiteSpace = " ".repeat(startDepth);
+        for (let i = 0; i < this.children.length; i++) {
+            let currChild = this.children[i];
+            switch (currChild.type) {
+                case TokenType.Open:
+                    text += currChild.text + currChild.formatChildren(startDepth + increment);
+                    break;
+                case TokenType.Close:
+                    text += currChild.text;
+                    break;
+                case TokenType.Fact:
+                    text += " " + currChild.text;
+                    break;
+                case TokenType.Newline:
+                    text += currChild.text + leadingWhiteSpace;
+                    break;
+                case TokenType.Start:
+                    break;
+            }
+        }
+        return text;
+    }
+
     /* Close doesn't look right */
     print(): string {
         return this.text + this.children.map(child => child.print()).join(" ");
@@ -57,12 +83,12 @@ export class Node {
 }
 
 function parseToken(token: string): Node {
-    if(isBlockOpenChar(token)) {
+    if (isBlockOpenChar(token)) {
         return new Node(TokenType.Open, token);
     } else if (isBlockEndChar(token)) {
         return new Node(TokenType.Close, token);
     } else if (isWhiteSpaceChar(token)) {
-        return new Node(TokenType.Whitespace, token);
+        return new Node(TokenType.Newline, token);
     } else {
         return new Node(TokenType.Fact, token);
     }
@@ -70,8 +96,8 @@ function parseToken(token: string): Node {
 
 function tokenize(text: string): string[] {
     return text.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ').
-                replace(/\{/g, ' { ').replace(/\}/g, ' } ').
-                replace(/\[/g, ' [ ').replace(/\]/g, ' ] ').trim().split(/[\t|' ']+/);
+        replace(/\{/g, ' { ').replace(/\}/g, ' } ').
+        replace(/\[/g, ' [ ').replace(/\]/g, ' ] ').trim().split(/[\t|' ']+/);
 }
 
 export function parseText(text: string): Node {
@@ -79,17 +105,17 @@ export function parseText(text: string): Node {
     let _ = 0;
     let tokens = tokenize(text);
     [parentNode.children, _] = parse(parentNode, tokens, 0);
-    let s = parentNode.print();
+    let s = parentNode.formatChildren(0);
     return parentNode;
 }
 
 export function parse(node: Node, tokens: string[], index: number): [Node[], number] {
     let nodes: Node[] = [];
-    for(let i = index; i < tokens.length; i++) {
+    for (let i = index; i < tokens.length; i++) {
         let newNode = parseToken(tokens[i]);
-        if(newNode.type === TokenType.Open) {
+        if (newNode.type === TokenType.Open) {
             [newNode.children, i] = parse(newNode, tokens, i + 1);
-        } else if(newNode.type === TokenType.Close) {
+        } else if (newNode.type === TokenType.Close) {
             nodes.push(newNode);
             return [nodes, i];
         }
